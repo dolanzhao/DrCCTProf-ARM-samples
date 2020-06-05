4c107e5118a7c16d67144bab682626c90d5f1553
Ubuntu <ubuntu@ip-172-31-44-192.ec2.internal>
2020-06-05 20:31:58 +0000

    support init DrCCTProf

--- DrCCTProf/init.sh
+++ DrCCTProf/init.sh
@@ -2,9 +2,9 @@
 
 CUR_DIR=$(cd "$(dirname "$0")";pwd)
 
-# cd $CUR_DIR
-# wget https://github.com/dolanzhao/DrCCTProf/archive/v1.0.0.tar.gz
-# tar -zxvf v1.0.0.tar.gz
-# rm v1.0.0.tar.gz
+cd $CUR_DIR
+wget https://github.com/dolanzhao/DrCCTProf/archive/v1.0.0.tar.gz
+tar -zxvf v1.0.0.tar.gz
+rm v1.0.0.tar.gz
 
 $CUR_DIR/DrCCTProf-1.0.0/release_version_build.sh
