e525a55169bf215758898b77f5d5c9bb7d342fef
dolanzhao <neuzqd@gmail.com>
2020-08-15 01:31:53 +0000

    update scripts

--- gromacs/build.sh
+++ gromacs/build.sh
@@ -7,12 +7,9 @@ if [ ! -d ${BUILD} ]; then
     mkdir ${BUILD}
 fi
 
-cd $CUR_DIR
-mkdir build
-cd build
+cd $BUILD
 cmake $SRC -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_MPI=ON
-make
+make -j CFLAGS=-fPIC
 make check
 sudo make install
 source /usr/local/gromacs/bin/GMXRC
-
--- lammps/init.sh
+++ lammps/init.sh
@@ -6,3 +6,5 @@ cd $CUR_DIR
 wget https://github.com/lammps/lammps/archive/r15061.tar.gz
 tar -zxvf r15061.tar.gz
 rm r15061.tar.gz
+
+$CUR_DIR/build.sh
